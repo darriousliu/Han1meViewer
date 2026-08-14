cef5cc05253977bcdc3c9c122ae8c843251a073a
Darrious Liu <60057825+darriousliu@users.noreply.github.com>
2026-08-14 20:02:15 +0800

    build(kmp): 对齐多平台构建工具链

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,12 +2,16 @@
 plugins {
     alias(libs.plugins.com.android.application) apply false
     alias(libs.plugins.com.android.library) apply false
+    alias(libs.plugins.android.kotlin.multiplatform.library) apply false
+    alias(libs.plugins.kotlin.multiplatform) apply false
+    alias(libs.plugins.kotlin.jvm) apply false
     alias(libs.plugins.org.jetbrains.kotlin.plugin.serialization) apply false
     alias(libs.plugins.org.jetbrains.kotlin.plugin.parcelize) apply false
     alias(libs.plugins.com.google.gms.google.services) apply false
     alias(libs.plugins.com.google.firebase.crashlytics) apply false
     alias(libs.plugins.com.google.firebase.firebase.pref) apply false
     alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.compose.multiplatform) apply false
     id("com.mikepenz.aboutlibraries.plugin") version "14.2.0" apply false
 }
 
